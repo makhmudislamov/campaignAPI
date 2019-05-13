7600975d25a299ea252bcc959ccdfde6b317b564
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-13 12:27:21 -0700

    making request to color api

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,2 +1,12 @@
-// javascript/scripts.js
-alert("hello world");
+// scripts.js
+
+// Make a request to the color api
+axios.get('http://www.thecolorapi.com/id?hex=24B1E0')
+    .then(function (response) {
+        // handle success
+        alert(response.data.hex.value);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    });
