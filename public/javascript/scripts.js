71908e5bb13f4eea04ea66f0385da9fb78c44440
Makhmud Islamov <sunnatovichvv@gmail.com>
2019-05-13 12:34:23 -0700

    adding axios.post

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -1,12 +1,42 @@
 // scripts.js
 
-// Make a request to the color api
-axios.get('http://www.thecolorapi.com/id?hex=24B1E0')
-    .then(function (response) {
-        // handle success
-        alert(response.data.hex.value);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
+// listen for a form submit event
+// prevent the default form behavior
+// serialize the form data into an object
+// use axios to initialize a post request and send in the form data
+// wait for the success response from the server
+// remove the information from the form
+// display the data as a new comment on the page
+// handle any errors
+
+
+// Only run this if we find the new-comment element
+if (document.getElementById('new-campaign')) {
+    // listen for a form submit event
+    document.getElementById("new-campaign").addEventListener("submit", e => {
+        // prevent the default form behavior
+        e.preventDefault();
+
+        // serialize the form data into an object
+        let campaign = {};
+        const inputs = document.getElementsByClassName('form-control');
+        for (var i = 0; i < inputs.length; i++) {
+            campaign[inputs[i].name] = inputs[i].value;
+        }
+
+        // use axios to initialize a post request and send in the form data
+
+        axios.post('/campaigns', campaign)
+            .then(function (response) {
+                // wait for the success response from the server
+                console.log(response);
+                // remove the information from the form
+                // display the data as a new comment on the page
+            })
+            .catch(function (error) {
+                console.log(error);
+                // handle any errors
+                alert('There was a problem saving your campaign. Please try again.')
+            });
     });
+}
